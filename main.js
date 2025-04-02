541d00f37f1d919bfb025c91595c564c85a90b98
makowithyou <mako.withyou@gmail.com>
2025-04-02 14:09:48 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-$( "#date" ).datepicker();
+$( "#date" ).datepicker({
+    dateFormat: "yy/mm/dd",
+    dayNamesMin: [ "月", "火", "水", "木", "金", "土", "日" ],
+    monthNames: [ "1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月" ],
+    showMonthAfterYear: true,
+    yearSuffix: "年",
+});
