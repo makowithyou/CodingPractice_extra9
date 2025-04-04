108a6f3ec53a5ea1fde798fea98ce4415be1c937
makowithyou <mako.withyou@gmail.com>
2025-04-04 13:22:16 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+// Datepickerでカレンダー機能
 $( "#date" ).datepicker({
     dateFormat: "yy/mm/dd",
     dayNamesMin: [ "月", "火", "水", "木", "金", "土", "日" ],
     monthNames: [ "1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月" ],
     showMonthAfterYear: true,
     yearSuffix: "年",
+});
+
+// ラジオボタンでテキストの切り替え
+$("#online").click(function(){
+    $(".online_details").toggle();
+    $(".offline_details").toggle();
+});
+$("#offline").click(function(){
+    $(".online_details").toggle();
+    $(".offline_details").toggle();
 });
